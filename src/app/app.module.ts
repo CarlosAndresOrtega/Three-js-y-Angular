250edd2f5e5ed8eb7b1f913df27b33afdf86dd7b
CarlosAndresOrtega <caortegayate@gmail.com>
2021-01-05 22:02:34 -0500

    practica de reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RenderComponent } from './render/render.component';
@@ -25,7 +25,8 @@ import { FormularioComponent } from './formulario/formulario.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  userForm = new FormGroup({
+    email: new FormControl( '', Validators.compose([Validators.email, Validators.required])),
+    //telefono: new FormControl( '', Validators.compose([Validators.minLength(10), Validators.required])),
+    telefono: new FormControl('', Validators.compose([Validators.pattern(/^[+]+[0-9]+$/),Validators.required, Validators.maxLength(20), Validators.minLength(7)])),
+    name: new FormControl('', Validators.compose([Validators.pattern(/^[a-zA-Z ]+$/),Validators.required])),
+    lname: new FormControl('', Validators.compose([Validators.pattern(/^[a-zA-Z ]+$/),Validators.required])),
+    username: new FormControl('', Validators.compose([Validators.maxLength(20),Validators.required, Validators.minLength(6)])),
+    password: new FormControl('', Validators.compose([Validators.minLength(6),Validators.required])),
+    vpassword: new FormControl('',Validators.compose([Validators.minLength(6),Validators.required])),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  enviar(values){
+    // alert(values.JSON);
+    console.log(values);
+    
+  }
 }
